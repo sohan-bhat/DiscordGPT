0a84b474e7240c9ecf6039cbbd15ce450683c593
sohan-bhat <64918912+sohan-bhat@users.noreply.github.com>
2023-07-05 16:11:02 -0500

    Fixed bot error issue and changed and updated token in .env file and added max_tokens param to .createChatCompletion

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const { Client, IntentsBitField } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
 const keepAlive = require("./server")
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
 client.on('ready', () => {
-    console.log('The bot is online!');
+  console.log('The bot is online!');
 });
 
 const configuration = new Configuration({
-    apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-    if (message.channel.id !== process.env.CHANNEL_ID) return;
-    if (message.content.startsWith('?')) {
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+  if (message.content.startsWith('?')) {
 
         let conversationLog = [
             { role: 'system', content: 'You are a friendly chatbot.' },
@@ -35,40 +35,38 @@ client.on('messageCreate', async (message) => {
             prevMessages.reverse();
 
             prevMessages.forEach((msg) => {
-                if (msg.content.startsWith('!')) return;
-                if (msg.author.id !== client.user.id && message.author.bot) return;
-                if (msg.author.id == client.user.id) {
-                    conversationLog.push({
-                        role: 'assistant',
-                        content: msg.content,
-                        name: msg.author.username
-                            .replace(/\s+/g, '_')
-                            .replace(/[^\w\s]/gi, ''),
-                    });
-                }
+            if (msg.content.startsWith('!')) return;
+            if (msg.author.id !== client.user.id && message.author.bot) return;
+            if (msg.author.id == client.user.id) {
+                conversationLog.push({
+                role: 'assistant',
+                content: msg.content,
+                name: msg.author.username
+                    .replace(/\s+/g, '_')
+                    .replace(/[^\w\s]/gi, ''),
+                });
+            }
 
-                if (msg.author.id == message.author.id) {
-                    conversationLog.push({
-                        role: 'user',
-                        content: msg.content,
-                        name: message.author.username
-                            .replace(/\s+/g, '_')
-                            .replace(/[^\w\s]/gi, ''),
-                    });
-                }
+            if (msg.author.id == message.author.id) {
+                conversationLog.push({
+                role: 'user',
+                content: msg.content,
+                name: message.author.username
+                    .replace(/\s+/g, '_')
+                    .replace(/[^\w\s]/gi, ''),
+                });
+            }
             });
 
             const result = await openai
-                .createChatCompletion({
-                    model: 'gpt-3.5-turbo',
-                    messages: conversationLog,
-                    max_tokens: 1000,
-                    prompt: "i will be funny and friendly and silly most of the time also be helpful when someone needs help with anything"
-                    // max_tokens: 256, // limit token usage
-                })
-                .catch((error) => {
-                    console.log(`OPENAI ERR: ${error}`);
-                });
+            .createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: conversationLog,
+                max_tokens: 256, // limit token usage
+            })
+            .catch((error) => {
+                console.log(`OPENAI ERR: ${error}`);
+            });
             message.reply(result.data.choices[0].message);
         } catch (error) {
             console.log(`ERR: ${error}`);
@@ -80,4 +78,3 @@ client.on('messageCreate', async (message) => {
 
 keepAlive()
 client.login(process.env.TOKEN);
-
